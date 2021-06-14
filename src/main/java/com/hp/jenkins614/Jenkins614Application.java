89a7d307838613804292457c4841bad13d108070
QinCao <1250056060@qq.com>
2021-06-14 10:39:18 +0800

    第一次更新

---
+++ src/main/java/com/hp/jenkins614/Jenkins614Application.java
@@ -0,0 +1,13 @@
+package com.hp.jenkins614;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins614Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkins614Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hp/jenkins614/Jenkins614ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hp.jenkins614;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins614ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
