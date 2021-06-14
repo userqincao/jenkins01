011537461d974a654c0e0b221633aa39d2753070
QinCao <1250056060@qq.com>
2021-06-14 10:43:51 +0800

    第二次更新

--- src/main/java/com/hp/jenkins614/controller/HelloController.java
+++ src/main/java/com/hp/jenkins614/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello(){
         return "Hello Jenkins ！！！";
     }
+
+    @GetMapping("/no")
+    public String no(){
+        return "No Jenkins ！！！";
+    }
 }
